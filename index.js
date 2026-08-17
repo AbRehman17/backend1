bddadc944ce734df725b670d1c8b3c9bd87538e5
Muhammad Hussain AbdulRehman <p.rehman2005@gmail.com>
2026-08-17 15:36:41 +0500

    sdsds

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 import express from 'express'
+
 const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello! Welcome to my Express server 👋')
+})
+
 app.listen(8000, () => {
   console.log('App running on port 8000')
 })
